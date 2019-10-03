e582721be0a2e7d3eb1ce3832f12c691b48e5f58
Jim McStanton <jim@jhmcstanton.com>
2019-10-02 23:13:12 -0500

    Added verilog output test for Vec 1 bit.
    Confirms behavior seen in issue #810.

--- tests/shouldwork/TopEntity/PortNames.hs
+++ tests/shouldwork/TopEntity/PortNames.hs
@@ -24,6 +24,31 @@ import Clash.Explicit.Testbench
 topEntity :: (Signal System Int, (Signal System Int, Signal System Int))
 topEntity = (pure 0, (pure 1, pure 2))
 
+{-# ANN bitEntity
+  (Synthesize
+    { t_name     = "PortNames_bitEntity"
+    , t_inputs   =
+        [ PortName "inp0"
+        , PortName "inp1"
+        , PortName "inp2"
+        , PortName "inp3"
+        , PortName "inp4"
+        , PortName "inp5"
+        , PortName "inp6"
+        ]
+    , t_output = PortName "outp0"
+    }) #-}
+bitEntity
+  :: Bit
+  -> BitVector 1
+  -> Unsigned 1
+  -> Vec 1 Bit
+  -> Vec 1 (BitVector 1)
+  -> Vec 1 (Unsigned 1)
+  -> Vec 1 (Signed 1)
+  -> Bool
+bitEntity _ _ _ _ _ _ _ = True
+
 -- Simulation test
 {-# ANN testBench
   (Synthesize
@@ -51,8 +76,21 @@ mainVerilog = do
   [topDir] <- getArgs
   content <- readFile (takeDirectory topDir </> "PortNames_topEntity" </> "PortNames_topEntity.v")
 
---  content <- readFile $ hdlDir </> "verilog/PortNames/PortNames_topEntity/PortNames_topEntity.v"
-
   assertIn "top_zero" content
   assertIn "top_sub_one" content
   assertIn "top_sub_two" content
+
+vec1bitVerilog :: IO ()
+vec1bitVerilog = do
+  [topDir] <- getArgs
+  content <- readFile (takeDirectory topDir </> "PortNames_bitEntity" </> "PortNames_topEntity.v")
+
+  mapM_ (`assertIn` content)
+    [ "input inp0"
+    , "input [0:0] inp1"
+    , "input [0:0] inp2"
+    , "input [0:0] inp3"
+    , "input [0:0] inp4"
+    , "input [0:0] inp5"
+    , "input [0:0] inp6"
+    ]
